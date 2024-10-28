388ba06ca27c110d0979cc18f6b8589c8b24f1cb
manuel <mauel.ortiz@ceiba.com.co>
2024-10-28 11:52:26 -0500

    .

--- src/main/java/com/monolito/modular/modules/customer/application/mappers/CustomerMapper.java
+++ src/main/java/com/monolito/modular/modules/customer/application/mappers/CustomerMapper.java
@@ -8,27 +8,7 @@ import com.monolito.modular.modules.customer.domain.entities.Customer;
 @Mapper(componentModel = "spring")
 public interface CustomerMapper {
 
-  
   CustomerDTO toDto(Customer customer);
 
   Customer toEntity(CustomerDTO dto);
 }
-
-
-// @Mapper(componentModel = "spring")
-// public interface AdmisionResumenMapper {
-
-//   @Mapping(target = "id", source = "admision.id")
-//   @Mapping(target = "consecutivo", source = "admision.consecutivo")
-//   @Mapping(target = "nombrePaciente", source = "admision.paciente.nombre")
-//   @Mapping(target = "apellidosPaciente", source = "admision.paciente.apellidos")
-//   @Mapping(target = "procedimiento", source = "admision.procedimiento")
-//   @Mapping(target = "codigoProcedimiento", source = "admision.codigoProcedimiento")
-//   @Mapping(target = "fechaInicio", source = "admision.fechaInicio")
-//   @Mapping(target = "cantidadSesiones", source = "admision.cantidadSesiones")
-//   @Mapping(target = "sesionesAsignadas", source = "sesionesAsignadas")
-
-//   AdmisionResumenDTO toDto(Admision admision, long sesionesAsignadas);
-
-//   Admision toEntity(AdmisionResumenDTO dto);
-// }
--- src/main/java/com/monolito/modular/modules/customer/domain/services/interfaces/ICustomerService.java
+++ src/main/java/com/monolito/modular/modules/customer/domain/services/interfaces/ICustomerService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import com.monolito.modular.modules.customer.domain.entities.Customer;
 
 public interface ICustomerService {
-Customer create(Customer customer);
+  
+  List<Customer> findAll();
 
-List<Customer> findAll();
+  Customer create(Customer customer);
+
 }
